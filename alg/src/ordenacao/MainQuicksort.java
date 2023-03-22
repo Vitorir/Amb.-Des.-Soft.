0bdde4e166f95bb7e811879f698ac4d4d6a67980
Vitorir <vitorireboucas@edu.unifor.br>
2023-03-22 18:51:09 -0300

    Add Quicksort Algorithm

---
+++ alg/src/ordenacao/MainQuicksort.java
@@ -0,0 +1,29 @@
+package ordenacao;
+import java.util.*;
+
+public class MainQuicksort {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Digite a quantidade de numeros a serem ordenados: ");
+        int n = sc.nextInt();
+        int[] vetor = new int[n];
+        
+        Quicksort q = new Quicksort();
+
+        for(int i = 0; i < vetor.length; i++){
+            vetor[i] = sc.nextInt();
+        }
+
+        System.out.println("Desordenado");
+        for(int i = 0; i < vetor.length; i++){
+            System.out.print(vetor[i] + " ");
+        }
+        
+        q.quicksort(vetor, 0, vetor.length - 1);
+        
+        System.out.println("\n\nOrdenado");
+        for(int i = 0; i < vetor.length; i++){
+            System.out.print(vetor[i] + " ");
+        }
+    }
+}
